0638217778116650e16f5d595beb38134bcfbe60
uo270927 <Tiempo2018>
2022-06-24 12:48:04 +0200

    Added "What is *page name*?" and "Why *page name?" to about page

--- webapp/src/views/AboutView.vue
+++ webapp/src/views/AboutView.vue
@@ -3,11 +3,42 @@
     <h1>Conoce *Nombre de la página*</h1>
     <article>
       <h2>¿Qué es *Nombre de la página*?</h2>
-      <p>Texto describiendolo</p>
+      <p>
+        Mediante *Nombre de la página* todos los usuarios podrán realizar su
+        investigación y búsquedas, viendo la forma en la que se invierten
+        diversas subvenciones del estado (obteniendo los datos actualizados a
+        través de datos oficiales de la web del gobierno).
+      </p>
+      <p>
+        Mantenido por nuestros desarrolladores, cualquiera puede contribuir al
+        proyecto, ya sea mediante sus propias sugerencias que acogeremos
+        cálidamente así como desarrollando directamente en el proyecto (ya que
+        es un proyecto Open Source en el que cualquiera puede participar y
+        acceder al código fuente).
+      </p>
     </article>
     <article>
       <h2>¿Por qué *Nombre de la página*?</h2>
-      <p>Texto describiendolo</p>
+      <p>
+        Para poder hablar de *nombre de la página* tenemos que remontarnos a su
+        origen, en la empresa Empathy.CO.
+      </p>
+      <p>
+        Desde la empresa de Empathy.CO buscamos el mejor servicio para nuestros
+        clientes, haciendo que estos sean dueños de sus propios datos y tratando
+        de garantizar una experiencia divertida, con confianza y entendiendo al
+        cliente (haciendo referencia a los 3 principios básicos de la empresa:
+        "Trust", "Joy" y "Understand").
+      </p>
+      <p>
+        Con este trasfondo, junto con los recién integrantes de un proyecto de
+        formación protagonizado por la empresa y la universidad de Oviedo,
+        Academy Project, y con la idea de Ángel Maldonado, CEO y cofundador de
+        la empresa, surge el concepto de hacer una aplicación transparente,
+        accesible y útil para todos. Así nace *Nombre de la página*, como un
+        artefacto para alcanzar con mayor facilidad ese acceso acceso a la
+        información que debería estar en manos de todos.
+      </p>
     </article>
     <article>
       <h2>Conoce a los desarrolladores</h2>
