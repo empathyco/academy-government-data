b1464ef7868575ff19f3c0451145e7b463b44c98
Daniel Barrientos Iglesias <UO270162@uniovi.es>
2022-07-01 11:25:58 +0200

    Update csvParser.py

--- search-plugin/index-governmentdata-parser/csvParser.py
+++ search-plugin/index-governmentdata-parser/csvParser.py
@@ -2,27 +2,23 @@ import csv
 import json
 
 
-def parseTsv(tsvRoute, jsonRoute):
+def parseTsv(tsvRoute):
 
     try:
         reader = csv.DictReader(open(tsvRoute), delimiter="\t")
 
-        with open(jsonRoute, 'w') as jsonFile:
+        with open("jsonOutput.json", 'w') as jsonFile:
             jsonFile.write(json.dumps(list(reader)))
     except:
         print("Error with the route, please try again")
 
 def parserInterface():
     tsvRoute = ""
-    jsonRoute = ""
 
     while (tsvRoute == ""):
         tsvRoute = input("Enter a suitable route for the TSV file:\n")
 
-    while (jsonRoute == ""):
-        jsonRoute = input("Enter a suitable route for the JSON file:\n")
-
-    parseTsv(tsvRoute, jsonRoute)
+    parseTsv(tsvRoute)
 
 
 if __name__ == "__main__":
